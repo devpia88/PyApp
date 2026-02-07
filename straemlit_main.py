e3b4d8fbc10c1549da24116a69e17259e5ce0c8f
devpia88 <devpia88@gmail.com>
2026-02-07 11:34:33 +0000

    코드변경1

--- straemlit_main.py
+++ straemlit_main.py
@@ -1,4 +1,23 @@
 import streamlit as st
 
-st.title("Hello Streamlit")
+st.title("안녕하세요 Streamlit")
+
+st.header("헤더를 입력할 수 있어요. :sunglasses:")
+
+st.subheader("이것은 subheader 입니다.")
+
+st.text("일반적인 텍스트 출력")
+
+st.caption("캡션을 넣어봅니다.")
+
+sample_code ='''
+def_function():
+    print('hello, word')
+'''
+
+st.text(sample_code)
+st.code(sample_code)
+
+
+
 
